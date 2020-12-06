1cbf74a511ab6a9efb7715269655831ab9f2c7ec
Gabriel Molter <gjmolter@Gabriel-Molters-MacBook.local>
2020-12-06 13:05:41 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'revo-calendar'
+import 'revo-calendar/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
