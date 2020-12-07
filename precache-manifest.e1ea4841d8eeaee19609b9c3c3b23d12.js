a56bc5a61a9b9071436a2e07fcf37ebaf116fd00
Gabriel Molter <gjmolter@Gabriel-Molters-MacBook.local>
2020-12-07 19:41:27 -0300

    Updates

---
+++ precache-manifest.e1ea4841d8eeaee19609b9c3c3b23d12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21cdc46e0e3f06533b957041202973e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "494a95707657642042e4",
+    "url": "./static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "4c3f03e81508243015be",
+    "url": "./static/css/main.8a32d8fc.chunk.css"
+  },
+  {
+    "revision": "494a95707657642042e4",
+    "url": "./static/js/2.70de5a7a.chunk.js"
+  },
+  {
+    "revision": "7b208ac56efc718dfeb5953f78657d2f",
+    "url": "./static/js/2.70de5a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3f03e81508243015be",
+    "url": "./static/js/main.59bd2c99.chunk.js"
+  },
+  {
+    "revision": "82bcda1788bcc666ebf4",
+    "url": "./static/js/runtime-main.4202c793.js"
+  }
+]);
